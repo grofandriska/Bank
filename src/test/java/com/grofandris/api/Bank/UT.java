ad901473e5cb680c88a1a55e670a19f1c32fcda9
Gróf András <andrasgrof93@gmail.com>
2021-10-26 12:38:09 +0200

    test cases implemented

--- src/test/java/com/grofandris/api/Bank/UT.java
+++ src/test/java/com/grofandris/api/Bank/UT.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
@@ -64,9 +63,10 @@ public class UT {
         user.setId(1L);
         Account account = new Account(1L, null, "OTP", 10000.00);
         account.setUser(user);
-
+        accountService.saveAccount(account);
         Account result = testRestTemplate.postForObject(baseUrl + "/save", account, Account.class);
-        when(accountService.withdrawFromBalance(result.getId(), 2000.00)).thenReturn(8000.00);
+        Account compareTo = testRestTemplate.getForObject(baseUrl + "/getById/" + 1L, Account.class);
+        when(accountService.withdrawFromBalance(compareTo.getId(), 2000.00)).thenReturn(8000.00);
 
 
     }
@@ -84,5 +84,46 @@ public class UT {
         Account result = testRestTemplate.postForObject(baseUrl + "/save", account, Account.class);
         when(accountService.getBalance(result.getId())).thenReturn(10000.00);
 
+    }
+
+    @Test
+    public void depositMoneyAddToBalance() {
+        User user = new User();
+        user.setEmail("mail@gmail.com");
+        user.setId(null);
+        user.setUserName("Feri");
+        user.setId(1L);
+        Account account = new Account(1L, null, "OTP", 10000.00);
+        account.setUser(user);
+
+        Account result = testRestTemplate.postForObject(baseUrl + "/save", account, Account.class);
+
+        when(accountService.addToBalance(result.getId(), 2000.00)).thenReturn(12000.00);
+
+    }
+
+    @Test
+    public void transferMoneyThrowsRuntimeException() {
+        User user = new User();
+        user.setEmail("mail@gmail.com");
+        user.setId(null);
+        user.setUserName("Feri");
+        user.setId(1L);
+        Account account = new Account(1L, null, "OTP", 0.00);
+        account.setUser(user);
+
+        Account result = testRestTemplate.postForObject(baseUrl + "/save", account, Account.class);
+
+        User userSecond = new User();
+        user.setEmail("mail@gmail.com");
+        user.setId(null);
+        user.setUserName("Feri");
+        user.setId(1L);
+        Account accountSecond = new Account(2L, null, "OTP", 10000.00);
+        account.setUser(userSecond);
+
+        Account resultSecond = testRestTemplate.postForObject(baseUrl + "/save", accountSecond, Account.class);
+
+        when(accountService.transferMoney(1L, 2L, 1000.00)).thenThrow(RuntimeException.class);
     }
 }
